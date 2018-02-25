4de35125eaa05f3ddb754aca6419a01d4d42e16a
Vlad <35502476+butterfIy@users.noreply.github.com>
2018-02-25 03:30:22 +0100

    Update
    Check more ports and kill process.

--- Anti-Miner/Program.cs
+++ Anti-Miner/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Anti_Miner
@@ -13,41 +11,53 @@ namespace Anti_Miner
         {
             string[] PIDS = new string[100];
             int pids_int = 0;
-            using (Process p = new Process())
+
+            Process p = new Process();
+
+            ProcessStartInfo ps = new ProcessStartInfo();
+            ps.Arguments = "-a -n -o -p TCP";
+            ps.FileName = "netstat.exe";
+            ps.UseShellExecute = false;
+            ps.WindowStyle = ProcessWindowStyle.Hidden;
+            ps.RedirectStandardInput = true;
+            ps.RedirectStandardOutput = true;
+            ps.RedirectStandardError = false;
+
+            p.StartInfo = ps;
+            p.Start();
+
+            StreamReader output = p.StandardOutput;
+            string netstat_out = output.ReadToEnd();
+
+            string[] line = Regex.Split(netstat_out, "\r\n");
+
+            for (int i = 4; i != line.Length - 2; i++)
             {
+                string[] port = Regex.Split(line[i], "\\s+");
 
-                ProcessStartInfo ps = new ProcessStartInfo();
-                ps.Arguments = "-a -n -o -p TCP";
-                ps.FileName = "netstat.exe";
-                ps.UseShellExecute = false;
-                ps.WindowStyle = ProcessWindowStyle.Hidden;
-                ps.RedirectStandardInput = true;
-                ps.RedirectStandardOutput = true;
-                ps.RedirectStandardError = false;
-
-                p.StartInfo = ps;
-                p.Start();
-
-                StreamReader output = p.StandardOutput;
-
-                string netstat_out = output.ReadToEnd();
-
-                string[] line = Regex.Split(netstat_out, "\r\n");
-
-                for (int i = 4; i != line.Length - 2; i++)
-                {
-
-                    string[] port = Regex.Split(line[i], "\\s+");
-
-                    if (port[3].Contains("3333"))
-                    {
-                        PIDS[pids_int] = port[5];
-                        pids_int++;
-                    }
+                if (port[3].Contains("3333") && port[3].Contains("7777") && port[3].Contains("4444") && port[3].Contains("5555") && port[3].Contains("6666") && port[3].Contains("45550") && port[3].Contains("45560") && port[3].Contains("45570") && port[3].Contains("45590") && port[3].Contains("45790")) {
+                    PIDS[pids_int] = port[5];
+                    pids_int++;
                 }
             }
 
-            Console.WriteLine(PIDS[0]);
+            for (int c = 0; c != pids_int; c++) {
+                Process p2 = new Process();
+
+                ProcessStartInfo ps2 = new ProcessStartInfo();
+                ps2.Arguments = " /PID "+ PIDS[c];
+                ps2.FileName = "taskkill.exe";
+                ps2.UseShellExecute = false;
+                ps2.WindowStyle = ProcessWindowStyle.Hidden;
+                ps2.RedirectStandardInput = true;
+                ps2.RedirectStandardOutput = true;
+                ps2.RedirectStandardError = false;
+
+                p2.StartInfo = ps2;
+                p2.Start();
+
+                Console.WriteLine("Founded and killed.");
+            }
             Console.ReadLine();
         }
     }
